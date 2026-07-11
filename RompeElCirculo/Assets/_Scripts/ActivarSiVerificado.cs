04b33875f3ca27d5e37c112a44ca84e1f2a9c56f
DonMario-Git <mariorrd.11@gmail.com>
2026-07-11 10:33:49 -0500

    11.07.2026

--- RompeElCirculo/Assets/_Scripts/ActivarSiVerificado.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ActivarSiVerificado : MonoBehaviour
-{
-    private void OnEnable()
-    {
-        //gameObject.SetActive(AppManager.UserData.verificado);
-    }
-}
